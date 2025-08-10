01cc3bae257fc6f3076996df841d6bc54d6a741e
Lord Nibbler <200654593+monsteradeliciosnah@users.noreply.github.com>
2025-08-10 16:42:08 -0400

    Lint/format: autoflake, ruff (E,F,I,E7), isort, black

--- neural_chaos_lab_max/esn.py
+++ neural_chaos_lab_max/esn.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class ESN:
     def __init__(self, n_inputs, n_res=500, spectral_radius=0.9, alpha=0.3, seed=42):
         rng = np.random.default_rng(seed)
@@ -14,19 +15,25 @@ class ESN:
     def fit(self, U, Y, washout=100, reg=1e-6):
         N = U.shape[0]
         X = np.zeros((N, self.W.shape[0]))
-        for t in range(1,N):
-            X[t] = (1-self.alpha)*X[t-1] + self.alpha*np.tanh(self.Win @ U[t] + self.W @ X[t-1])
+        for t in range(1, N):
+            X[t] = (1 - self.alpha) * X[t - 1] + self.alpha * np.tanh(
+                self.Win @ U[t] + self.W @ X[t - 1]
+            )
         Xtr = X[washout:]
         Ytr = Y[washout:]
         # ridge
-        self.Wout = np.linalg.solve(Xtr.T @ Xtr + reg*np.eye(Xtr.shape[1]), Xtr.T @ Ytr)
+        self.Wout = np.linalg.solve(
+            Xtr.T @ Xtr + reg * np.eye(Xtr.shape[1]), Xtr.T @ Ytr
+        )
         return self
 
     def predict(self, U, x0=None):
         N = U.shape[0]
         X = np.zeros((N, self.W.shape[0])) if x0 is None else x0
         Yhat = np.zeros((N, self.Wout.shape[1]))
-        for t in range(1,N):
-            X[t] = (1-self.alpha)*X[t-1] + self.alpha*np.tanh(self.Win @ U[t] + self.W @ X[t-1])
+        for t in range(1, N):
+            X[t] = (1 - self.alpha) * X[t - 1] + self.alpha * np.tanh(
+                self.Win @ U[t] + self.W @ X[t - 1]
+            )
             Yhat[t] = X[t] @ self.Wout
         return Yhat
